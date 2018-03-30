770e5cbb082c66ea3c09e2e086b8c3913ee279ce
Jaures <jauresade@gmail.com>
2018-03-30 16:50:27 -0700

    Added Computer/Desktop Version

---
+++ fsDetect.py
@@ -0,0 +1,52 @@
+
+import cv2 # OpenCV: Open Computer Vision
+from time import sleep  # Just for sleep function
+
+# Select Your Webcam
+vid_cap = cv2.VideoCapture(0);
+
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+smileCascade = cv2.CascadeClassifier("haarcascade_smile.xml")
+
+
+while True:
+    ret, frame = vid_cap.read()
+    grayed = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect Faces
+    faces = faceCascade.detectMultiScale(
+        grayed,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+    # Detect Smiles
+    smiles = smileCascade.detectMultiScale(
+        grayed,
+        scaleFactor=1.1,
+        minNeighbors=80,
+        minSize=(40,40),
+        flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+    print("Found: {0} faces, {1} smiles\n".format(len(faces),len(smiles)))
+
+    # Draw Faces
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Draw Smiles
+    for (x,y,w,h) in smiles:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
+        
+
+    cv2.imshow("Feed", frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+vid_cap.release()
+cv2.destroyAllWindows()
