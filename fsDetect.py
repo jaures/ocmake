11f0758533f67be761ef7bb65611540e35d8c594
jaures <jaures@users.noreply.github.com>
2018-04-14 09:05:05 -0700

    Update fsDetect.py
    Adding email capabilities

--- fsDetect.py
+++ fsDetect.py
@@ -3,6 +3,19 @@ from time import sleep
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+### Modify For Email Sending
+fromaddr = "cs101f17@gmail.com" 
+pswd = "learning4lyfe"
+toaddr = "your@email.com"
+###
+
+
 # Set up PiCamera object
 piCam = PiCamera()
 piCam.resolution = (640,480)
@@ -59,6 +72,61 @@ while True:
     cv2.imshow("Feed", frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imwrite("frame.jpg",frame);
+        # instance of MIMEMultipart
+        msg = MIMEMultipart()
+
+        # storing the senders email address  
+        msg['From'] = fromaddr
+
+        # storing the receivers email address 
+        msg['To'] = toaddr
+
+        # storing the subject 
+        msg['Subject'] = "Subject of the Mail"
+
+        # string to store the body of the mail
+        body = "RPI-Zero Pic Snap!"
+
+        # attach the body with the msg instance
+        msg.attach(MIMEText(body, 'plain'))
+
+        # open the file to be sent 
+        filename = "frame.jpg"
+        attachment = open("frame.jpg", "rb")
+
+        # instance of MIMEBase and named as p
+        p = MIMEBase('application', 'octet-stream')
+
+        # To change the payload into encoded form
+        p.set_payload((attachment).read())
+
+        # encode into base64
+        encoders.encode_base64(p)
+
+        p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+        # attach the instance 'p' to instance 'msg'
+        msg.attach(p)
+
+        # creates SMTP session
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+
+        # start TLS for security
+        s.starttls()
+
+        # Authentication
+        s.login(fromaddr, pswd)
+
+        # Converts the Multipart msg into a string
+        text = msg.as_string()
+
+        # sending the mail
+        s.sendmail(fromaddr, toaddr, text)
+
+        # terminating the session
+        s.quit()
+        
         break
 
 # When everything is done, release the capture
